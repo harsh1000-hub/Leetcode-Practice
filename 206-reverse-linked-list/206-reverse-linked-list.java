525b726c44bdee38194598f97c37ed416625370b
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2022-02-10 17:51:35 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (10.04%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -10,13 +10,16 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev=null;
+        ListNode dummy_Node=null;
         while(head!=null){
-            ListNode next=head.next;
-            head.next=prev;
-            prev=head;
-            head=next;
+            ListNode next=head.next;  // store the address of the next node of head
+            head.next=dummy_Node; // assign head.next=null in first iteration and here we break the link b/w head and head.next
+            dummy_Node=head;  // assign dummy_Node to head
+            head=next;  // move head for further steps using Line 15 next bcz next maintain the address of head.next 
         }
-        return prev;
+        return dummy_Node;
     }
 }
+// Input : head->1->2->3->4->5->null
+// changing the links in Line 25
+// Output: null<-1<-2<-3<-4<-5<-head
