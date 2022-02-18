ac5f83b4d072adc09dcd5881b8c0b53ac82dcb76
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2022-02-18 17:37:15 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (22.40%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -16,12 +16,14 @@ class Solution {
         ListNode slow=head;
         ListNode fast=head;
         while(fast.next!=null){
-            if(fast.next.next==null){
-                slow=slow.next;
+            if(fast.next.next==null){  // condition for when there are one middle node
+                slow=slow.next; 
                 fast=fast.next;
             }
-            else{slow=slow.next;
-            fast=fast.next.next;}
+            else{   // condition for when there are two middle nodes
+                slow=slow.next;    
+                fast=fast.next.next;
+            }
         }
         return slow;
     }
