427737deeced0dd60b0b2746f26e7590b40e7909
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2022-04-13 00:02:20 +0530

    Time: 15 ms (6.52%), Space: 42.8 MB (8.88%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,13 +1,15 @@
 class Solution {
     public String toLowerCase(String s) {
-        StringBuilder str=new StringBuilder(s);
+        String str="";
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch>='A' && ch<='Z'){
                 char lower_case=(char)('a'+ch-'A');
-                str.setCharAt(i,lower_case);
+                str+=lower_case;
+            }else{
+                str+=s.charAt(i);
             }
-        }
-        return str.toString();
+        } 
+        return str;
     }
 }
