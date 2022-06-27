359f4f62debc1294588a5386dcd8f18011f5be71
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2022-06-27 10:19:19 +0530

    Time: 2 ms (63.26%), Space: 41.2 MB (62.22%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==0){
-            return false;
-        }
-        if(n==1){
-            return true;
-        }
-        if(n%2==0){
-            return isPowerOfTwo(n/2);
+        if(n>0){
+            if((n & n-1)==0){
+                return true;
+            }
         }
         return false;
     }
