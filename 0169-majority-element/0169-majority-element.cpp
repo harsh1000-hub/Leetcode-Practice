3ac84421cd43dbd12b80edecc8b7cb872e3b4baf
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2023-02-14 21:41:13 +0530

    Time: 29 ms (25.93%), Space: 19.6 MB (75.85%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        if(nums.size()==0){
+            return nums[0];
+        }
+        int check_agent=nums.size()/2;
+        int result=0;
+        for(int i=0;i<nums.size();i++){
+            //inner loop
+            int count=1;
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]==INT_MIN){
+                    break;
+                }
+                else if(nums[i]==nums[j]){
+                    count++;
+                    nums[j]=INT_MIN;
+                }
+            }
+           // cout<<count<<" ";
+            if(count>check_agent){
+                result=nums[i];
+                check_agent=count;
+            }
+        }
+        return result;
+    }
+};
