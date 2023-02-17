9fe8f3dfac2e41ca4eec20a555b56cc05391fa3b
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2023-02-17 20:34:07 +0530

    Time: 11 ms (98.85%), Space: 18.1 MB (25.34%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int len=nums.size();
         int range=0;
         int sum_arr=0;
-        int arr[len+1];
-        for(int i=0;i<=len;i++){
-            if(range<=len){
+        int arr[nums.size()+1];
+        int sum_nums=0;
+        for(int i=0;i<=nums.size();i++){
+            if(range<=nums.size()){
                 arr[i]=range++;
                 sum_arr+=arr[i];
             }
-        }
-        //cout<<sum_arr<<endl;
-        int sum_nums=0;
-        for(int i=0;i<nums.size();i++){
-            sum_nums+=nums[i];
+            if(i<nums.size()){
+                sum_nums+=nums[i];
+            }
         }
          return sum_arr-sum_nums;
     }
