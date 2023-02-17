fa687156e96a9d82eabde14a89ae6be6bfaef46c
Harsh Vardhan <72147432+harsh1000-hub@users.noreply.github.com>
2023-02-17 20:41:12 +0530

    Time: 21 ms (67.93%), Space: 17.9 MB (94.61%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        /* 
+        1. Approach is simple first creata a array arr of size nums.size()+1
+        2. In sum_arr variable sum up all element from 0 to n.
+        3. In sum_nums variable sum up all element that are in nums vector
+        4. Last step return the sum_arr-sum_nums that is equal to missing element
+        
+        Test case -> nums=[3,0,1]
+        sum_nums=3+0+1 -> 4
+        arr that is consist of 0 to n elemenet like that 
+        arr=[0,1,2,3]
+        sum_arr=0+1+2+3 -> 6
+        
+        return sum_arr - sum_nums that is equal to 6-4 -> 2(missing element)
+        */
         int range=0;
         int sum_arr=0;
         int arr[nums.size()+1];
